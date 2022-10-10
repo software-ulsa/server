1aaca95be757d7421fa228c8debe1793b848d3b0
Rafael <logralahad@gmail.com>
2022-10-10 16:39:13 -0500

    rol deleted message updated

--- src/controllers/RolController.ts
+++ src/controllers/RolController.ts
@@ -68,7 +68,7 @@ export const deleteRol = async (req: Request, res: Response) => {
     if (rolDeleted.affected == 0)
       return res.status(404).json({ error: "Hubo un error al actualizar." });
 
-    return res.status(200).json({ message: "Rol actualizado correctamente." });
+    return res.status(200).json({ message: "Rol eliminado correctamente." });
   }
   return res.status(404).json({ error: "Rol no existe." });
 };
